26ca13cbc4a695e0bc82a608bfd2932009b49c11
Jackyxtt <944766685@qq.com>
2023-02-23 21:06:33 +0800

    save in tum format

--- MCVIO/src/utility/visualization.cpp
+++ MCVIO/src/utility/visualization.cpp
@@ -337,16 +337,13 @@ void pubOdometry(const MCVIOEstimator &estimator, const std_msgs::Header &header
     // write result to file
     ofstream foutC(VINS_RESULT_PATH, ios::app);
     foutC.setf(ios::fixed, ios::floatfield);
-    foutC.precision(0);
-    foutC << header.stamp.toSec() * 1e9 << ",";
     foutC.precision(5);
-    foutC << estimator.Ps[WINDOW_SIZE].x() << ","
-          << estimator.Ps[WINDOW_SIZE].y() << ","
-          << estimator.Ps[WINDOW_SIZE].z() << "," << tmp_Q.w() << ","
-          << tmp_Q.x() << "," << tmp_Q.y() << "," << tmp_Q.z() << ","
-          << estimator.Vs[WINDOW_SIZE].x() << ","
-          << estimator.Vs[WINDOW_SIZE].y() << ","
-          << estimator.Vs[WINDOW_SIZE].z() << "," << endl;
+    foutC << header.stamp.toSec() << " ";
+    foutC.precision(5);
+    foutC << estimator.Ps[WINDOW_SIZE].x() << " "
+          << estimator.Ps[WINDOW_SIZE].y() << " "
+          << estimator.Ps[WINDOW_SIZE].z() << " " << tmp_Q.w() << " "
+          << tmp_Q.x() << " " << tmp_Q.y() << " " << tmp_Q.z() << endl;
     foutC.close();
   }
 }
